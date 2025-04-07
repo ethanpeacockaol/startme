b4b41faa67980234703d23e85abcaac6c4c8cf12
ethanpeacockaol <ethanpeacock@aol.com>
2025-04-07 13:47:15 -0500

    server update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, send_from_directory
 import ssl
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return send_from_directory('static', 'index.html')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'index.html')
 
 if __name__ == '__main__':
-    # Replace 'path/to/your/cert.pem' and 'path/to/your/key.pem' with your certificate and private key paths.
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain('cert.pem', 'key.pem')
-    app.run(host='0.0.0.0', port=443, ssl_context=context)
+    context.load_cert_chain('cert.pem', 'key.pem')  # Replace with your certificate paths
+    app.run(host='0.0.0.0', port=8000, ssl_context=context, debug=True)
